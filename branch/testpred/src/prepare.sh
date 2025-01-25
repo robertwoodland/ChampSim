73d601f34b979edde92c8d3c40a047e01f1c5404
Robert Woodland <robertwoodland47@gmail.com>
2025-01-25 21:42:57 +0000

    Add script to move bsv outside of ChampSim

---
+++ branch/testpred/src/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Move the parent folder outside of the repository
+mv /home/robert/cam/part2/project/ChampSim-1/branch/testpred/src/ /home/robert/cam/part2/project/src/
+rm /home/robert/cam/part2/project/src/prepare.sh
