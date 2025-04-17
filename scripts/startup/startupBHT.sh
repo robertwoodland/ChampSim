44d5e8849acdaf35020713c11dbea12e35612d69
Robert Woodland <robertwoodland47@gmail.com>
2025-04-17 17:01:11 +0100

    Init instruction graphs

---
+++ scripts/startup/startupBHT.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TRACE="454.calculix-104B.champsimtrace.xz"
+
+# Run with warmup instructions from 10 to 10**7, and simulation instructions of 10**6
+for ((i=10; i<=10**7; i*=10)); do
+    BASE_COMMAND="./bin/champsim --warmup_instructions $i"
+    echo "Running: $BASE_COMMAND --simulation_instructions 1000"
+    echo $BASE_COMMAND "--simulation_instructions" 1000 $TRACE "&> results/startup/bht/${i}.txt"
+    $BASE_COMMAND "--simulation_instructions" 1000 $TRACE > "results/startup/bht/${i}.txt"
+done
