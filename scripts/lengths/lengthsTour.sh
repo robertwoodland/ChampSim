4c778a2a37af87931a99f4d44fcb67ec6640a21d
Robert Woodland <robertwoodland47@gmail.com>
2025-04-17 15:43:08 +0100

    Remove unnecessary echo

--- scripts/lengths/lengthsTour.sh
+++ scripts/lengths/lengthsTour.sh
@@ -7,7 +7,5 @@ TRACE="454.calculix-104B.champsimtrace.xz"
 # Loop through factors of 10 for simulation instructions
 for ((i=100; i<=10**7; i*=10)); do
     echo "Running: $BASE_COMMAND --simulation_instructions $i $TRACE"
-    echo $BASE_COMMAND "--simulation_instructions" $i $TRACE "&> results/lengthsTOUR/${i}.txt"
-    $BASE_COMMAND "--simulation_instructions" $i $TRACE > "results/lengthsTOUR/${i}.txt"
-    # date +%s
+    $BASE_COMMAND "--simulation_instructions" $i $TRACE > "results/lengths/lengthsTour/${i}.txt"
 done
