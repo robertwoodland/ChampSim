3823ba0623017abea2f7bd578fc9e9eb698429c7
Robert Woodland <robertwoodland47@gmail.com>
2025-05-01 11:27:21 +0100

    Flush UT

--- test/test_main.cc
+++ test/test_main.cc
@@ -277,7 +277,57 @@ TEST(PerceptronTests, PredPatternTest)
   printf("Total mispredictions: %ld\n", mispreds);
 }
 
-// Check performance under aliasing branch addresses
+// Test flush
+TEST(PerceptronTests, FlushTest)
+{
+
+  uint8_t out;
+  uint64_t ip = 1;
+  O3_CPU cpu;
+  cpu.initialize_branch_predictor();
+
+  // Warm up the predictor for two branches
+  for (uint64_t count = 1; count < 200; count++) {
+    // predict 1
+    out = cpu.predict_branch(ip);
+    // update 1
+    cpu.last_branch_result(ip, 0, 1, 3);
+    // predict 2
+    out = cpu.predict_branch(ip + 10);
+    // update 2
+    cpu.last_branch_result(ip + 10, 8, 0, 3);
+  }
+
+  // Assert that they trained correctly
+  // predict 1
+  out = cpu.predict_branch(ip);
+  EXPECT_EQ(out, 1);
+
+  // predict 2
+  out = cpu.predict_branch(ip + 10);
+  EXPECT_EQ(out, 0);
+
+  // Flush predictor
+  cpu.flush_branch_predictor();
+
+  // Check that the predictor is reset
+  for (uint64_t i = 0; i < 10; i++) {
+    // Train a little to 0 and 1 (flipped from before)
+    out = cpu.predict_branch(ip);
+    cpu.last_branch_result(ip, 0, 0, 3);
+
+    out = cpu.predict_branch(ip + 10);
+    cpu.last_branch_result(ip + 10, 8, 1, 3);
+  }
+
+  // Pred and assert we have unlearnt (successful flush)
+  out = cpu.predict_branch(ip);
+  EXPECT_EQ(out, 0);
+
+  out = cpu.predict_branch(ip + 10);
+  EXPECT_EQ(out, 1);
+}
+
 
 int main(int argc, char** argv)
 {
