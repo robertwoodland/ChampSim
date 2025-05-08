84b7bd60f4f44d1bd3c49a5174649cc2ff9b2e85
Robert Woodland <robertwoodland47@gmail.com>
2025-05-08 17:43:03 +0100

    Scripts for testing varying size params

---
+++ scripts/budget/global.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+
+# Read the Perceptron.bsv file and extract the required values
+PRED="perceptron"
+UPPER="Perceptron"
+PRED_FILE="branch/bsv/$PRED/$UPPER.bsv"
+FILE_NAME="../$UPPER.bsv"
+
+# 10^6 = 30s per trace
+BASE_COMMAND="./bin/champsim --warmup_instructions 1000000 --simulation_instructions 1000000"
+TRACE="454.calculix-104B.champsimtrace.xz"
+
+
+# Overall:
+# LOCAL=(2 2 2 5 5 10 10 11 12 13 15 16)
+# GLOBAL=(8 10 23 25 31 34 34 36 40 50 60 80)
+# COUNT=(11 19 19 33 64 91 182 341 680 1360 2720 5440)
+# # Change to 5, 31, 64 to showcase power of two
+
+# Budget tests:
+# LOCAL=(1 3 6 11 22 33 44)
+# GLOBAL=(36 36 36 36 36 36 36 36 36 36 36)
+# COUNT=(341 341 341 341 341 341 341 341 341 341 341 341)
+# # Fix all but Local, starting at budget of 16KB = (11 36 341)
+# # Double / halve each time
+
+LOCAL=(11 11 11 11 11 11 11 11 11 11 11)
+GLOBAL=(5 9 18 36 72 144 288)
+COUNT=(341 341 341 341 341 341 341 341 341 341 341 341)
+# Fix all but Global, starting at budget of 16KB = (11 36 341)
+# Double / halve each time
+
+# LOCAL=(11 11 11 11 11 11 11 11 11 11 11)
+# GLOBAL=(36 36 36 36 36 36 36 36 36 36 36)
+# COUNT=(43 85 171 341 642 1284 2568)
+# # Fix all but counts, starting at budget of 16KB = (11 36 341)
+# # Double / halve each time
+
+
+
+if [[ -f "$PRED_FILE" ]]; then
+    for i in {0..6}; do
+        SIZE=$(python3 ./scripts/histories/size.py $PRED ${LOCAL[i]} ${GLOBAL[i]} ${COUNT[i]})
+
+        # Change into build directory
+        cd "$(dirname "$PRED_FILE")/Build" || exit 1
+        echo "Size: ${SIZE}B, LOCAL: ${LOCAL[i]}, GLOBAL: ${GLOBAL[i]}, COUNT: ${COUNT[i]}"
+        OUTPUT_FILE="${SIZE}_${LOCAL[i]}_${GLOBAL[i]}_${COUNT[i]}.txt"
+        
+        # Modify params
+        sed -i "s/typedef\s\+\w\+\s\+PerceptronEntries;/typedef ${LOCAL[i]} PerceptronEntries;/" "$FILE_NAME"
+        sed -i "s/typedef\s\+\w\+\s\+PerceptronGHistEntries;/typedef ${GLOBAL[i]} PerceptronGHistEntries;/" "$FILE_NAME"
+        sed -i "s/typedef\s\+\w\+\s\+PerceptronCount;/typedef ${COUNT[i]} PerceptronCount;/" "$FILE_NAME"
+
+        echo "Making $PRED"
+        make all_bsim
+
+        echo "Changing to top"
+        # Change back to top
+        cd "-" || exit 1
+        
+        echo "Removing old champsim"
+        rm "./bin/champsim"
+        
+        echo "Making champsim"
+        make
+
+        echo "Running: $BASE_COMMAND $TRACE > results/budget/global/$OUTPUT_FILE"
+        $BASE_COMMAND $TRACE > "results/budget/global/$OUTPUT_FILE"
+    done
+else
+    echo "Error: $PRED_FILE not found."
+    exit 1
+fi
---
+++ scripts/budget/width.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+# Read the Perceptron.bsv file and extract the required values
+PRED="perceptron"
+UPPER="Perceptron"
+PRED_FILE="branch/bsv/$PRED/$UPPER.bsv"
+FILE_NAME="../$UPPER.bsv"
+
+# 10^6 = 30s per trace
+BASE_COMMAND="./bin/champsim --warmup_instructions 1000000 --simulation_instructions 1000000"
+TRACE="454.calculix-104B.champsimtrace.xz"
+
+
+LOCAL=(11 11 11 11 11 11 11 )
+GLOBAL=(36 36 36 36 36 36 36)
+COUNT=(341 341 341 341 341 341 341)
+# Fix all but width, starting at budget of 16KB = (11 36 341, 8)
+WEIGHTS=(5 6 7 8 9 10 11 12)
+# Vary by factors of two
+
+if [[ -f "$PRED_FILE" ]]; then
+    for WEIGHT in "${WEIGHTS[@]}"
+    do
+        # Change into build directory
+        cd "$(dirname "$PRED_FILE")/Build" || exit 1
+        
+        # Modify bit width
+        sed -i 's|typedef\s\+Vector#(TAdd#(PerceptronEntries, 1),\s\+Int#([^)]\+))\s\+PerceptronWeights;|typedef Vector#(TAdd#(PerceptronEntries, 1), '"Int#(${WEIGHT}"')) PerceptronWeights;|' "$FILE_NAME"
+        sed -i 's/typedef\s\+Vector#(PerceptronGHistEntries,\s\+Int#([^)]\+))\s\+PerceptronGWeights;/typedef Vector#(PerceptronGHistEntries, '"Int#(${WEIGHT}"')) PerceptronGWeights;/' "$FILE_NAME"
+
+        echo "Changing to top"
+        # Change back to top
+        cd "-" || exit 1
+
+        SIZE=$(python3 ./scripts/histories/size.py $PRED ${LOCAL[0]} ${GLOBAL[0]} ${COUNT[0]} ${WEIGHT})
+
+        # Change into build directory
+        cd "$(dirname "$PRED_FILE")/Build" || exit 1
+        echo "Size: ${SIZE}B, LOCAL: ${LOCAL[0]}, GLOBAL: ${GLOBAL[0]}, COUNT: ${COUNT[0]}, WIDTH:${WEIGHT}"
+        OUTPUT_FILE="${SIZE}_${LOCAL[0]}_${GLOBAL[0]}_${COUNT[0]}_${WEIGHT}.txt"
+        
+        # Modify params
+        sed -i "s/typedef\s\+\w\+\s\+PerceptronEntries;/typedef ${LOCAL[0]} PerceptronEntries;/" "$FILE_NAME"
+        sed -i "s/typedef\s\+\w\+\s\+PerceptronGHistEntries;/typedef ${GLOBAL[0]} PerceptronGHistEntries;/" "$FILE_NAME"
+        sed -i "s/typedef\s\+\w\+\s\+PerceptronCount;/typedef ${COUNT[0]} PerceptronCount;/" "$FILE_NAME"
+
+
+        echo "Making $PRED"
+        make all_bsim
+
+        echo "Changing to top"
+        # Change back to top
+        cd "-" || exit 1
+        
+        echo "Removing old champsim"
+        rm "./bin/champsim"
+        
+        echo "Making champsim"
+        make
+
+        echo "Running: $BASE_COMMAND $TRACE > results/budget/width/$OUTPUT_FILE"
+        $BASE_COMMAND $TRACE > "results/budget/width/$OUTPUT_FILE"
+    done
+else
+    echo "Error: $PRED_FILE not found."
+    exit 1
+fi
