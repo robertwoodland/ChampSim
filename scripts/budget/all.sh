d3a08811715d6ef36756d829a37bc59897afafe9
Robert Woodland <robertwoodland47@gmail.com>
2025-05-11 12:48:29 +0100

    403 gcc budget

---
+++ scripts/budget/all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TESTPRED_FILE="branch/testpred/testpred.cc"
+SCRIPT="branch/testpred/script.sh"
+
+if [[ -f "$TESTPRED_FILE" ]]; then
+    # Set script to point to perceptron
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim-1/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim-1/branch/bsv/perceptron/Build/mkTestbench_bsim.so|" "$SCRIPT"
+
+    ./scripts/budget/local.sh
+    ./scripts/budget/global.sh
+    ./scripts/budget/count.sh
+    ./scripts/budget/width.sh
+else
+    echo "Error: $TESTPRED_FILE not found."
+    exit 1
+fi
--- scripts/budget/local.sh
+++ scripts/budget/local.sh
@@ -9,48 +9,16 @@ FILE_NAME="../$UPPER.bsv"
 
 # 10^7 = 300s per trace
 BASE_COMMAND="./bin/champsim --warmup_instructions 10000000 --simulation_instructions 10000000"
-TRACE="454.calculix-104B.champsimtrace.xz"
-TRACE="401.bzip2-277B.champsimtrace.xz"
-
-# Overall:
-# LOCAL=(2 2 2 5 5 10 10 11 12 13 15 16)
-# GLOBAL=(8 10 23 25 31 34 34 36 40 50 60 80)
-# COUNT=(11 19 19 33 64 91 182 341 680 1360 2720 5440)
-# # Change to 5, 31, 64 to showcase power of two
-
-# Default yields:
-# CPU 0 cumulative IPC: 3.979 instructions: 1000001 cycles: 251297
-# CPU 0 Branch Prediction Accuracy: 99.49% MPKI: 0.462 Average ROB Occupancy at Mispredict: 351.8
-
-# Conditional Branch Prediction Accuracy %0.463
-
-# Count of 12 yields:
-# CPU 0 cumulative IPC: 3.953 instructions: 1000001 cycles: 252954
-# CPU 0 Branch Prediction Accuracy: 99.41% MPKI: 0.528 Average ROB Occupancy at Mispredict: 341.1
-
-# Conditional Branch Prediction Accuracy %0.55
-# Ie slightly worse
+# TRACE="454.calculix-104B.champsimtrace.xz"
+# TRACE="401.bzip2-277B.champsimtrace.xz"
+TRACE="403.gcc-17B.champsimtrace.xz"
 
 # Budget tests:
 LOCAL=(1 3 6 11 50 134 280)
 GLOBAL=(36 36 36 36 36 36 36)
 COUNT=(341 341 341 341 341 341 341 341 341 341 341 341)
 # Fix all but Local, starting at budget of 16KB = (11 36 341)
-# Double / halve each time
-
-# LOCAL=(11 11 11 11 11 11 11 11 11 11 11)
-# GLOBAL=(5 9 18 36 72 144 288)
-# COUNT=(341 341 341 341 341 341 341 341 341 341 341 341)
-# # Fix all but Global, starting at budget of 16KB = (11 36 341)
-# # Double / halve each time
-
-# LOCAL=(11 11 11 11 11 11 11 11 11 11 11)
-# GLOBAL=(36 36 36 36 36 36 36 36 36 36 36)
-# COUNT=(43 85 171 341 642 1284 2568)
-# # Fix all but counts, starting at budget of 16KB = (11 36 341)
-# # Double / halve each time
-
-
+# Double / halve each times
 
 if [[ -f "$PRED_FILE" ]]; then
     for i in {0..6}; do
