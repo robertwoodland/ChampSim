99b30027008de2996d07bf7b762dbaea5a281437
Robert Woodland <robertwoodland47@gmail.com>
2025-05-16 21:07:44 +0100

    Script to invoke pattern scripts

---
+++ scripts/pattern/all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set predictor
+
+TESTPRED_FILE="branch/testpred/testpred.cc"
+SCRIPT="branch/testpred/script.sh"
+
+
+
+if [[ -f "$TESTPRED_FILE" ]]; then
+    # Ensure test_mode
+    sed -i "s|//\s\+#define\s\+TEST_MODE|#define TEST_MODE|" "$TESTPRED_FILE"
+
+    # make
+    
+
+    # # Set script to point to predictor
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim-1/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim-1/branch/bsv/perceptron/Build/mkTestbench_bsim.so|" "$SCRIPT"
+    ./scripts/pattern/perceptron.sh
+    
+    # # BHT
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim-1/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim-1/branch/bsv/bht/Build/mkTestbench_bsim.so|" "$SCRIPT"
+    ./scripts/pattern/bht.sh
+    
+    # # Tour
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim-1/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim-1/branch/bsv/tour/Build/mkTestbench_bsim.so|" "$SCRIPT"
+    ./scripts/pattern/tour.sh
+fi
+# run
+
+# Repeat!
