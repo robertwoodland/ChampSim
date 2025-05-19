4bf30890b2103e0110177a0a0dc571ef12d3a3d5
Robert Woodland <robertwoodland47@gmail.com>
2025-05-19 12:17:40 +0100

    Redact more things

--- scripts/budget/all.sh
+++ scripts/budget/all.sh
@@ -5,7 +5,7 @@ SCRIPT="branch/testpred/script.sh"
 
 if [[ -f "$TESTPRED_FILE" ]]; then
     # Set script to point to perceptron
-    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim/branch/bsv/perceptron/Build/mkTestbench_bsim.so|" "$SCRIPT"
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/redacted/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/redacted/cam/part2/project/ChampSim/branch/bsv/perceptron/Build/mkTestbench_bsim.so|" "$SCRIPT"
 
     ./scripts/budget/local.sh
     ./scripts/budget/global.sh
--- scripts/traces/traces.sh
+++ scripts/traces/traces.sh
@@ -4,14 +4,14 @@ TESTPRED_FILE="branch/testpred/testpred.cc"
 SCRIPT="branch/testpred/script.sh"
 
 if [[ -f "$TESTPRED_FILE" ]]; then
-    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim/branch/bsv/perceptron/Build/mkTestbench_bsim.so|" "$SCRIPT"
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/redacted/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/redacted/cam/part2/project/ChampSim/branch/bsv/perceptron/Build/mkTestbench_bsim.so|" "$SCRIPT"
     ./scripts/traces/perceptron.sh
     echo "Changing"
-    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim/branch/bsv/bht/Build/mkTestbench_bsim.so|" "$SCRIPT"
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/redacted/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/redacted/cam/part2/project/ChampSim/branch/bsv/bht/Build/mkTestbench_bsim.so|" "$SCRIPT"
     ./scripts/traces/bht.sh
-    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim/branch/bsv/tour/Build/mkTestbench_bsim.so|" "$SCRIPT"
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/redacted/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/redacted/cam/part2/project/ChampSim/branch/bsv/tour/Build/mkTestbench_bsim.so|" "$SCRIPT"
     ./scripts/traces/tour.sh
-    sed -i "s|Bluesim::sim\s\+load\s\+/home/robert/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/robert/cam/part2/project/ChampSim/branch/bsv/perceptron/Build/mkTestbench_bsim.so|" "$SCRIPT"
+    sed -i "s|Bluesim::sim\s\+load\s\+/home/redacted/cam/part2/project/ChampSim/branch/bsv/[^ ]*|Bluesim::sim load /home/redacted/cam/part2/project/ChampSim/branch/bsv/perceptron/Build/mkTestbench_bsim.so|" "$SCRIPT"
 else
     echo "Error: $TESTPRED_FILE not found."
     exit 1
